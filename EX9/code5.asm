f6325e899fc535b08063375acf23e87f38a96646
subha <subha21506_bcs28@mepcoeng.ac.in>
2026-04-14 13:18:08 +0530

    Create code5.asm

---
+++ EX9/code5.asm
@@ -0,0 +1,22 @@
+            ORG 8000H
+
+START:
+            MOV DPTR,#9000H
+            MOVX A,@DPTR
+
+            ANL A,#0FH
+            MOV R0,A
+
+            MOVX A,@DPTR
+            ANL A,#F0H
+            SWAP A
+
+            MOV B,#08H
+            MUL AB
+
+            ADD A,R0
+
+            INC DPTR
+            MOVX @DPTR,A
+
+            END
